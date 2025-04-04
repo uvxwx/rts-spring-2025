073c9ec91a8468718ac76925c43241b23ca0822f
uvxwx <116727374+uvxwx@users.noreply.github.com>
2025-04-04 16:02:18 +0400

    Add files via upload

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,32 @@
-void setup() {
+#include <TaskManager.h>
 
+void setup()
+{
+	pinMode(3, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(6, OUTPUT);
+	pinMode(9, OUTPUT);
+	pinMode(10, OUTPUT);
+	
+	Tasks.add([] {
+		PORTD ^= 1 << 3;
+	})->startIntervalUsec(10000);        
+	Tasks.add([] {
+		PORTD ^= 1 << 5;
+	})->startIntervalUsec(1000);      
+	Tasks.add([] {
+		PORTD ^= 1 << 6;
+	})->startIntervalUsec(500);      
+	Tasks.add([] {
+		PORTB ^= 1 << 1;
+	})->startIntervalUsec(100);   
+	Tasks.add([] {
+		PORTB ^= 1 << 2;
+	})->startIntervalUsec(50);    
+	
 }
 
-void loop() {
-
+void loop()
+{
+	Tasks.update();
 }
