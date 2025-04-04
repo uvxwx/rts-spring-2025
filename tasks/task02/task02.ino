64f0fcda6a7d95ec4895c29ab25acc0a9b4cb9fb
uvxwx <116727374+uvxwx@users.noreply.github.com>
2025-04-04 16:12:25 +0400

    Add files via upload

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,32 +1,44 @@
-#include <TaskManager.h>
+volatile unsigned long long prev_time = 0;
+volatile unsigned long long seg_sum = 0;
+volatile unsigned long long seg_square_sum = 0;
+volatile unsigned long long seg_cnt = 0;
+const unsigned long long MAX_SEG_CNT = 1000;
 
 void setup()
 {
-	pinMode(3, OUTPUT);
-	pinMode(5, OUTPUT);
-	pinMode(6, OUTPUT);
-	pinMode(9, OUTPUT);
-	pinMode(10, OUTPUT);
+	Serial.begin(9600);
+	pinMode(3, INPUT);
+	attachInterrupt(digitalPinToInterrupt(3), handle_seg, HIGH);
+}
+
+void handle_seg()
+{
+	unsigned long long cur = micros();
+	unsigned long long seg = cur - prev_time;
 	
-	Tasks.add([] {
-		PORTD ^= 1 << 3;
-	})->startIntervalUsec(10000);        
-	Tasks.add([] {
-		PORTD ^= 1 << 5;
-	})->startIntervalUsec(1000);      
-	Tasks.add([] {
-		PORTD ^= 1 << 6;
-	})->startIntervalUsec(500);      
-	Tasks.add([] {
-		PORTB ^= 1 << 1;
-	})->startIntervalUsec(100);   
-	Tasks.add([] {
-		PORTB ^= 1 << 2;
-	})->startIntervalUsec(50);    
+	seg_sum += seg;
+	seg_square_sum += seg * seg;
+	seg_cnt++;
 	
+	prev_time = cur;
 }
 
 void loop()
 {
-	Tasks.update();
+	if (seg_cnt >= MAX_SEG_CNT) {
+		noInterrupts();
+		
+		float average = (float) seg_sum / seg_cnt;
+		float stddev = sqrt(((float) seg_square_sum / seg_cnt) - (average * average));
+		
+		Serial.print("Average: ");
+		Serial.print(average);
+		Serial.print(" us, stddev: ");
+		Serial.print(stddev);
+		Serial.println(" us");
+		seg_sum = 0;
+		seg_square_sum = 0;
+		seg_cnt = 0;
+		interrupts();
+	}
 }
